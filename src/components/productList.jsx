0925b6d5fdb17f3f0c3f3f14bd27c576be822805
Bukason20 <chukwuebukaedward20@gmail.com>
2025-12-19 12:16:17 +0100

    learnt useEffect and controlled inputs

--- src/components/productList.jsx
+++ src/components/productList.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import products from "../products";
 import ProductCard from "./productCard";
 
 function ProductList() {
+  const getProducts = async () => {
+    const response = await fetch("http://localhost:5000/products");
+    const data = await response.json();
+
+    console.log(response, data);
+  };
+
+  useEffect(function () {
+    getProducts();
+  }, []);
   return (
     <div>
       <div className="product-list">
--- src/pages/productForm.jsx
+++ src/pages/productForm.jsx
@@ -1,23 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ProductForm = () => {
+  const [productName, setProductName] = useState("Maggi");
+  const [productDescription, setProductDescription] = useState("");
+  const [productStatus, setProductStatus] = useState();
+
+  // const handleChange = (e) => {
+  //   setProductName(e.target.value);
+  //   console.log(productName);
+  // };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(productName, productDescription, productStatus);
+  };
   return (
     <div>
       <h1>Add a product</h1>
 
-      <form action="">
+      <form
+        style={{
+          backgroundColor: "#e7e4e4ff",
+          border: "1px solid blue",
+          width: "40%",
+          margin: "2rem auto",
+          padding: "3rem 2rem",
+        }}
+        onSubmit={handleSubmit}
+      >
         <div>
-          <label htmlFor="">Product Name</label>
-          <input type="text" placeholder="Put in a valid product name" />
+          <label htmlFor="" style={{ display: "block" }}>
+            Product Name
+          </label>
+          <input
+            style={{
+              width: "100%",
+              padding: "1rem",
+              border: "none",
+              outline: "none",
+            }}
+            type="text"
+            placeholder="Put in a valid product name"
+            required
+            value={productName}
+            onChange={(e) => setProductName(e.target.value)}
+          />
         </div>
 
         <div>
-          <label htmlFor="">Product Description</label>
+          <label style={{ display: "block" }}>Product Description</label>
           <input
+            style={{
+              width: "100%",
+              padding: "1rem",
+              border: "none",
+              outline: "none",
+            }}
             type="text"
             placeholder="Write a description about the product"
+            required
+            value={productDescription}
+            onChange={(e) => setProductDescription(e.target.value)}
           />
         </div>
+
+        <div>
+          <label style={{ display: "block" }}> Product Status</label>
+          <select
+            required
+            style={{
+              width: "100%",
+              padding: "1rem",
+              border: "none",
+              outline: "none",
+            }}
+            value={productStatus}
+            onChange={(e) => setProductStatus(e.target.value)}
+          >
+            <option value="" disabled selected>
+              Put the Stock Status
+            </option>
+            <option value={true}>In Stock</option>
+            <option value={false}>Out of Stock</option>
+          </select>
+        </div>
+
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
